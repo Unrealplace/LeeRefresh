aa391819f43fd039b877044f262cd79fb36db1f9
Yang Li <ly@qiekj.com>
2018-12-06 15:09:20 +0800

    Revert "Revert "12.66 西瓜""
    This reverts commit d77e03c48ef2381ff6c837f4841709d97ffdf780.

---
+++ LeeRefreshView/CollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.h
+//  LeeRefreshView
+//
+//  Created by Yang Li on 2018/12/6.
+//  Copyright © 2018 https://github.com/Unrealplace     如有问题可以联系：939428468. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeeRefreshView/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  LeeRefreshView
+//
+//  Created by Yang Li on 2018/12/6.
+//  Copyright © 2018 https://github.com/Unrealplace     如有问题可以联系：939428468. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end

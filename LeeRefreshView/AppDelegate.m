d77e03c48ef2381ff6c837f4841709d97ffdf780
Yang Li <ly@qiekj.com>
2018-12-06 15:08:36 +0800

    Revert "12.66 西瓜"
    This reverts commit 251187023ff5a4b0bfdc3710e7b5f67aee28bc14.

--- LeeRefreshView/AppDelegate.m
+++ LeeRefreshView/AppDelegate.m
@@ -22,13 +22,14 @@
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-  
-    
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 

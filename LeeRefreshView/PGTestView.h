f72b6b02846044679445ecbb10472a3a48ba6b18
Yang Li <ly@qiekj.com>
2018-12-06 15:27:01 +0800

    12.7 新开发的版本

---
+++ LeeRefreshView/PGTestView.h
@@ -0,0 +1,17 @@
+//
+//  PGTestView.h
+//  LeeRefreshView
+//
+//  Created by Yang Li on 2018/12/6.
+//  Copyright © 2018 https://github.com/Unrealplace     如有问题可以联系：939428468. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PGTestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeeRefreshView/PGTestView.m
@@ -0,0 +1,21 @@
+//
+//  PGTestView.m
+//  LeeRefreshView
+//
+//  Created by Yang Li on 2018/12/6.
+//  Copyright © 2018 https://github.com/Unrealplace     如有问题可以联系：939428468. All rights reserved.
+//
+
+#import "PGTestView.h"
+
+@implementation PGTestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
